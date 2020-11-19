9f2fc79cc7057797333c87434967cb9b7393c116
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2020-11-19 11:22:22 +0100

    Création des classes métiers (vides)

---
+++ src/main/java/lyon1/iutinfo/cvda/projet/models/Projet.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lyon1.iutinfo.cvda.projet.models;
+
+/**
+ *
+ * @author Bastien BOMBARDELLA
+ */
+public class Projet {
+    
+}
