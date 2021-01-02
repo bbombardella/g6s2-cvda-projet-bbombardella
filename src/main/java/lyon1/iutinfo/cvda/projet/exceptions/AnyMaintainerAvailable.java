d70aa668a90f134d6b29f798bf7996f8dc7ab61a
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2021-01-02 22:11:23 +0100

    Création des exceptions personnalisées

---
+++ src/main/java/lyon1/iutinfo/cvda/projet/exceptions/AnyMaintainerAvailable.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lyon1.iutinfo.cvda.projet.exceptions;
+
+/**
+ *
+ * @author Bastien BOMBARDELLA
+ */
+public class AnyMaintainerAvailable extends Exception {
+    public AnyMaintainerAvailable() {
+        super("Le projet ne contient pas de maintainer dans la liste des membres.");
+    }
+}
---
+++ src/main/java/lyon1/iutinfo/cvda/projet/exceptions/NegativeNumberOfCommits.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lyon1.iutinfo.cvda.projet.exceptions;
+
+/**
+ *
+ * @author Bastien BOMBARDELLA
+ */
+public class NegativeNumberOfCommits extends Exception {
+    public NegativeNumberOfCommits() {
+        super("Le nombre de commits ne peut pas être négatif.");
+    }
+}
